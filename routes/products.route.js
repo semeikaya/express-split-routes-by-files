e4f4ef17f150788a367465641638b98b97593295
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-08 11:11:38 +0300

    Update products.route.js

--- routes/products.route.js
+++ routes/products.route.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/products', productsController.getMessages);
 router.post('/products', productsController.createMessage);
 router.delete('/products/:id', productsController.deleteMessage);
+router.patch('/products/:id', productsController.patchMessage);
+router.get('/products', productsController.getMessages);
 
 module.exports = router;
